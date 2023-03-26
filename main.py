8d51ebefa0b7d337ad6fccad17323e5a5eaa3361
Balumys <123511478+Balumys@users.noreply.github.com>
2023-03-27 02:21:33 +0300

    Added if main to main.py

--- main.py
+++ main.py
@@ -4,21 +4,21 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from date_functions import get_years_since_foundation_text
 from excel_db import get_product_data
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+if __name__ == "__main__":
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-template = env.get_template('template.html')
+    template = env.get_template('template.html')
 
+    rendered_page = template.render(
+        year=get_years_since_foundation_text(),
+        products=get_product_data()
+    )
 
-rendered_page = template.render(
-    year=get_years_since_foundation_text(),
-    products=get_product_data()
-)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
