5968e9e3c3f8a2ea723862d05f1837f89ce7c24a
Balumys <123511478+Balumys@users.noreply.github.com>
2023-03-27 03:26:44 +0300

    Changed for loop to dict comprehension

--- excel_db.py
+++ excel_db.py
@@ -7,9 +7,6 @@ def get_product_data(file, field_names):
                              keep_default_na=False)
     grouped_df = wines_df.groupby('Category')
 
-    wines = {}
-
-    for name, group in grouped_df:
-        wines[name] = group.to_dict(orient="records")
+    wines = {name: group.to_dict(orient="records") for name, group in grouped_df}
 
     return wines
